22439bb5e90b1ec4ef19a7dde6c22abd67b71b81
tvhuy99-web <tvhuy99@gmail.com>
2026-09-08 23:05:30 +0700

    fix: load map icon path patch before game

--- src/level13-bootstrap.js
+++ src/level13-bootstrap.js
@@ -1,6 +1,7 @@
 define([
 	'game/helpers/ui/AccessibilityDetailedErrorHelper',
-	'game/helpers/ui/AccessibilityDirectErrorPopupHelper'
+	'game/helpers/ui/AccessibilityDirectErrorPopupHelper',
+	'game/helpers/ui/AccessibilityMapIconPathFixHelper'
 ], function () {
 	require(['level13-app']);
 });
