c9198229bd9306559edcec70c7f4b2e5ebce16aa
tvhuy99-web <tvhuy99@gmail.com>
2026-09-09 00:56:51 +0700

    Load movement warning priority patch before startup

--- src/level13-bootstrap.js
+++ src/level13-bootstrap.js
@@ -7,16 +7,19 @@ define([
 		'game/helpers/ui/AccessibilityMapIconPathFixHelper',
 		'game/helpers/ui/AccessibilityOverviewRefreshPatch',
 		'game/helpers/ui/AccessibilityScoutActionablePatch',
-		'game/helpers/ui/AccessibilityMovementPositionHelper'
+		'game/helpers/ui/AccessibilityMovementPositionHelper',
+		'game/helpers/ui/AccessibilityMovementPositionPriorityPatch'
 	], function (
 		AccessibilityDetailedErrorHelper,
 		AccessibilityDirectErrorPopupHelper,
 		AccessibilityMapIconPathFixHelper,
 		AccessibilityOverviewRefreshPatch,
 		AccessibilityScoutActionablePatch,
-		AccessibilityMovementPositionHelper
+		AccessibilityMovementPositionHelper,
+		AccessibilityMovementPositionPriorityPatch
 	) {
-		if (AccessibilityMovementPositionHelper) new AccessibilityMovementPositionHelper();
+		let MovementPositionHelper = AccessibilityMovementPositionPriorityPatch || AccessibilityMovementPositionHelper;
+		if (MovementPositionHelper) new MovementPositionHelper();
 		require(['level13-app']);
 	});
 });
