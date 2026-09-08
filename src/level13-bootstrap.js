86e40ee657dbd646c32460d1e51d750450b367bb
tvhuy99-web <tvhuy99@gmail.com>
2026-09-09 00:51:18 +0700

    Start movement position warning helper independently

--- src/level13-bootstrap.js
+++ src/level13-bootstrap.js
@@ -6,8 +6,17 @@ define([
 		'game/helpers/ui/AccessibilityDirectErrorPopupHelper',
 		'game/helpers/ui/AccessibilityMapIconPathFixHelper',
 		'game/helpers/ui/AccessibilityOverviewRefreshPatch',
-		'game/helpers/ui/AccessibilityScoutActionablePatch'
-	], function () {
+		'game/helpers/ui/AccessibilityScoutActionablePatch',
+		'game/helpers/ui/AccessibilityMovementPositionHelper'
+	], function (
+		AccessibilityDetailedErrorHelper,
+		AccessibilityDirectErrorPopupHelper,
+		AccessibilityMapIconPathFixHelper,
+		AccessibilityOverviewRefreshPatch,
+		AccessibilityScoutActionablePatch,
+		AccessibilityMovementPositionHelper
+	) {
+		if (AccessibilityMovementPositionHelper) new AccessibilityMovementPositionHelper();
 		require(['level13-app']);
 	});
 });
