25a1fc6b3b1b5b6da538d10e241cfb018c3e9466
tvhuy99-web <tvhuy99@gmail.com>
2026-09-08 21:46:04 +0700

    Use a fresh bootstrap config for the localized site

---
+++ src/config-vi.js
@@ -0,0 +1,32 @@
+require.config({
+
+	deps: ["level13-app"],
+
+	waitSeconds: 30,
+
+	baseUrl: 'src',
+
+	paths: {
+		brejep: "../lib/brejep",
+		ash: "../lib/ash/ash.min",
+		jquery: "../lib/jquery",
+		lzstring: "../lib/lzstring",
+        json: "../lib/requirejs/json",
+		utils: "utils",
+		game: "game",
+	},
+
+	config: {
+		'level13-app': {
+			'version': "0.7.1",
+			'isDebugVersion': false,
+			'isCheatsEnabled': false,
+			'isDebugOutputEnabled': false,
+			'isAutosaveEnabled': true,
+			'isTrackingEnabled': true,
+		}
+	},
+	
+	urlArgs: "v=0.7.2",
+
+});
